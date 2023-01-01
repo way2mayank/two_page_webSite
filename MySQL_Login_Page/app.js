91499b6978804b9a9787b676e37dbd7c4d7b98f7
Mayank Srivastava <way2mayankshri@gmail.com>
2023-01-02 00:33:49 +0530

    done

--- MySQL_Login_Page/app.js
+++ MySQL_Login_Page/app.js
@@ -1,31 +1,35 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const mysql = require("mysql");
 const app = express();
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.set('view engine', 'html');
+app.set("view engine", "html");
+
+
 const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+  host: "localhost",
+  user: 'root',
+  password: '',
+  database: "sql_login",
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("MYSQL CONNECTED")
-    }
-})
-// Define Routes
-app.use('/', require('./routes/pages'));
-app.use('/auth', require('./routes/auth'));
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("MYSQL CONNECTED");
+  }
+});
 
-app.listen(5000)
+
+// Define Routes
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
+
+app.listen(5000);
